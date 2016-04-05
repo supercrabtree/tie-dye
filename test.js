2dc9c83b0d4c81f6674036fedf53f3fcbd721878
George Crabtree <supercrabtree@googlemail.com>
2016-04-05 08:58:57 +0200

    Improve test descriptions

--- test.js
+++ test.js
@@ -5,12 +5,12 @@ import { mapValues } from 'lodash/fp';
 const roundTo2Decimal = (x) => Math.round(x*100)/100;
 const roundObject = mapValues(roundTo2Decimal);
 
-test('rgbToHsl', t => {
+test('rgbToHsl correctly converts', t => {
   t.same(roundObject(rgbToHsl(128, 255, 0)), { h:0.25,  s:1,   l:0.5  });
   t.same(roundObject(rgbToHsl(234, 12, 35)), { h:0.98, s:0.9, l:0.48 });
 });
 
-test('hslToRgb', t => {
+test('hslToRgb correctly converts', t => {
   t.same(roundObject(hslToRgb(343, 0.1,  0.5 )), { r:140.25, g:114.75, b:121.98 });
   t.same(roundObject(hslToRgb(223, 0.22, 0.45)), { r:89.51,  g:103.81, b:140    });
 });
